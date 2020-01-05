f917beea4a74c4f5d48f71635495e96a2d5e2050
Gavin Mogan <git@gavinmogan.com>
2020-01-05 00:33:28 -0800

    Upgrade to write errors to stdout for k8s to report back errors. Also allow disabling attach/detach

--- commands.go
+++ commands.go
@@ -3,58 +3,78 @@ package flexvolume
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
+	"os"
 )
 
+// Commands is a list of actions supported by this flex volume
 func Commands(fv FlexVolume) []cli.Command {
-	return []cli.Command{
-		{
-			Name:  "init",
-			Usage: "Initialize the driver",
-			Action: func(c *cli.Context) error {
-				return handle(fv.Init())
-			},
+	var commands []cli.Command
+	commands = append(commands, cli.Command{
+		Name:  "init",
+		Usage: "Initialize the driver",
+		Action: func(c *cli.Context) error {
+			var resp Response = fv.Init()
+			var caps Capabilities = fv.Capabilities()
+			// Format the output as JSON.
+			output, err := json.Marshal(struct {
+				Response
+				Capabilities Capabilities `json:"capabilities,omitempty"`
+			}{resp, caps})
+
+			if err != nil {
+				return handle(WrapError(errors.Wrap(err, "Unable to create json")))
+			}
+			fmt.Println(string(output))
+			return nil
 		},
-		{
+	})
+
+	if fv.Capabilities().Attach {
+		commands = append(commands, cli.Command{
 			Name:  "attach",
 			Usage: "Attach the volume",
 			Action: func(c *cli.Context) error {
 				var opts map[string]string
 				if err := json.Unmarshal([]byte(c.Args().Get(0)), &opts); err != nil {
-					return err
+					return handle(WrapError(errors.Wrapf(err, "Unable to process json: %s", c.Args().Get(0))))
 				}
 
 				return handle(fv.Attach(opts))
 			},
-		},
-		{
+		})
+	}
+	if fv.Capabilities().Detach {
+		commands = append(commands, cli.Command{
 			Name:  "detach",
 			Usage: "Detach the volume",
 			Action: func(c *cli.Context) error {
 				return handle(fv.Detach(c.Args().Get(0)))
 			},
-		},
-		{
-			Name:  "mount",
-			Usage: "Mount the volume",
-			Action: func(c *cli.Context) error {
-				var opts map[string]string
-
-				if err := json.Unmarshal([]byte(c.Args().Get(2)), &opts); err != nil {
-					return err
-				}
-
-				return handle(fv.Mount(c.Args().Get(0), c.Args().Get(1), opts))
-			},
-		},
-		{
-			Name:  "unmount",
-			Usage: "Mount the volume",
-			Action: func(c *cli.Context) error {
-				return handle(fv.Unmount(c.Args().Get(0)))
-			},
-		},
+		})
 	}
+	commands = append(commands, cli.Command{
+		Name:  "mount",
+		Usage: "Mount the volume",
+		Action: func(c *cli.Context) error {
+			var opts map[string]string
+
+			if err := json.Unmarshal([]byte(c.Args().Get(2)), &opts); err != nil {
+				return handle(WrapError(errors.Wrapf(err, "Unable to process json: |%s|", c.Args().Get(2))))
+			}
+
+			return handle(fv.Mount(c.Args().Get(0), c.Args().Get(1), opts))
+		},
+	})
+	commands = append(commands, cli.Command{
+		Name:  "unmount",
+		Usage: "Mount the volume",
+		Action: func(c *cli.Context) error {
+			return handle(fv.Unmount(c.Args().Get(0)))
+		},
+	})
+	return commands
 }
 
 // The following handles:
@@ -66,6 +86,16 @@ func handle(resp Response) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(output))
+	os.Stderr.Write(output)
+	if resp.Status != StatusSuccess {
+		os.Exit(1)
+	}
 	return nil
+}
+
+func WrapError(err error) Response {
+	return Response{
+		Status:  StatusFailure,
+		Message: fmt.Sprintf("%s", err),
+	}
 }
--- types.go
+++ types.go
@@ -1,14 +1,20 @@
 package flexvolume
 
+// Status is a status
 type Status string
 
 const (
-	StatusSuccess      Status = "Success"
-	StatusFailure      Status = "Failure"
+	// StatusSuccess is a Status meaning success
+	StatusSuccess Status = "Success"
+	// StatusFailure is a Status meaning failure
+	StatusFailure Status = "Failure"
+	// StatusNotSupported is a Status meaning something isn't supported
 	StatusNotSupported Status = "Not Supported"
 )
 
+// FlexVolume is
 type FlexVolume interface {
+	Capabilities() Capabilities
 	Init() Response
 	Attach(map[string]string) Response
 	Detach(string) Response
@@ -16,8 +22,15 @@ type FlexVolume interface {
 	Unmount(string) Response
 }
 
+// Response is
 type Response struct {
 	Status  Status `json:"status"`
 	Message string `json:"message"`
 	Device  string `json:"device,omitempty"`
+}
+
+// Capabilities is a list of capabilities
+type Capabilities struct {
+	Attach bool `json:"attach"`
+	Detach bool `json:"detach"`
 }
